d03f5854ea079e0120fb1e05a1df44321ed8ce1c
Gideon Oba <deyonoba@gmail.com>
2025-03-30 14:24:04 +0100

    Add logging module 'setup_logging.py'

---
+++ src/utils/setup_logging.py
@@ -0,0 +1,79 @@
+
+import logging
+import logging.config
+from colorama import init as init_colorama
+from colorama import Fore, Back, Style
+
+
+init_colorama(wrap=False)
+
+
+class LevelBaseFormatter(logging.Formatter):
+    def __init__(self):
+        super().__init__()
+        self.datefmt = "%Y-%m-%d %H:%M:%S"
+        self.default_format = "%(asctime)s:%(levelname)s %(message)s"
+
+    def get_format(self, record: logging.LogRecord) -> str:
+        dt_fmt = f"{Style.DIM + Fore.GREEN}%(asctime)s{Fore.RESET}"
+        trace_stack_fmt = f"{Fore.BLUE}%(name)s{Fore.RESET}:[%(filename)s:%(module)s:{Fore.BLUE}%(lineno)s{Fore.RESET}]"
+        debug_level_fmt = f"{Style.NORMAL + Fore.CYAN}{record.levelname} {Fore.RESET}"
+        info_level_fmt = f"{Style.NORMAL + Fore.GREEN}{record.levelname} {Fore.RESET}"
+        error_level_fmt = f"{Style.NORMAL + Fore.RED}{record.levelname} {Fore.RESET}"
+
+        formats = {
+            logging.DEBUG: f"{dt_fmt}: {debug_level_fmt} {trace_stack_fmt} %(message)s",
+            logging.INFO: f"{dt_fmt}: {info_level_fmt} %(message)s",
+            logging.ERROR: f"{dt_fmt}: {error_level_fmt} {trace_stack_fmt} %(message)s"
+        }        
+        return formats.get(record.levelno, self.default_format)     
+
+    def format(self, record: logging.LogRecord):
+        log_format = self.get_format(record)
+        formatter = logging.Formatter(log_format, self.datefmt)
+        return formatter.format(record)
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "formatters": {
+        "level_based": {"()": LevelBaseFormatter},
+        "default": {
+            "format": "%(asctime)s: %(levelname)s %(name)s:[%(filename)s:%(module)s:%(lineno)s] %(message)s" 
+        }
+    },
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "level_based",
+            "level": "DEBUG",
+            "stream": "ext://sys.stdout"
+        },
+        "api_file_handler": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "src/logs/api.log",
+            "mode": "a",
+            "maxBytes": 1000,
+            "backupCount": 5, # Roll over log files :: api.log.1 to api.log.5
+            "formatter": "default"
+        }
+    },
+    "loggers": {
+        "fastapi": {
+            "level": "DEBUG",
+            "handlers": ["console", "api_file_handler"],
+            "propagate": False,
+        }
+    }
+}
+
+if __name__ == "__main__":
+    logging.config.dictConfig(LOGGING_CONFIG)
+
+    logger = logging.getLogger("fastapi")
+
+    logger.info("INFO")
+    logger.debug("DEBUG")
+    logger.error("ERROR")
